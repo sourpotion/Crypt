b7384030997d9a107e9988f452310a9c995e4420
DDDSchool <97126717@st.deltion.nl>
2026-02-12 15:30:47 +0100

    plrController and uiLogic update

--- Assets/Scripts/UiLogic.cs
+++ Assets/Scripts/UiLogic.cs
@@ -7,19 +7,26 @@ using UnityEngine.UI;
 
 public class UiLogic : MonoBehaviour
 {
+    // i thing when using remeber settings and quitToMenu is diffent in startschrem scene and RealGameScene kk else i needed to check or it is in game
+
     [Header("Must")]
-    public GameObject startSchrem; // Where it Start
+    public GameObject startSchrem; // Where it Start or where the PauseScreenStart (;
     public GameObject loadingScreenUi; //frame of the ui
     public Slider progessBar; // slider of the progessBar
 
+    //[Header("Could")]
+
+    [Header("Settings")]
+    public bool inGame;
+
     private GameObject currentUi; 
-    private String startSceneName = "SampleScene"; // here go the name of the startScene
+    private String[] sceneNames = {"SampleScene", "SampleScene"}; // id of the scene first is 0 sconf is 1 so on
+    private bool gameIsPause = false; //so when it is pause with esc u can go out
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         currentUi = startSchrem;
-        currentUi.SetActive(true);
 
         if (currentUi == null) { Debug.LogWarning("u need to do a public startSchrem AUB"); } //{} is more readable* 
     }
@@ -27,7 +34,21 @@ public class UiLogic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (inGame)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape)) {TogglePauseGame();}
+        }
+    }
+
+    public void TogglePauseGame()
+    {
+        currentUi.SetActive(false);
+
+        if (!gameIsPause) {Time.timeScale = 0;} else {Time.timeScale = 1;} //pause the game*
+        startSchrem.SetActive(!gameIsPause); // it can just deactive a deavtive ui*
+        currentUi = startSchrem;
+
+        gameIsPause = !gameIsPause; //toggle this
     }
 
     IEnumerator LoadScene(string sceneToLoad)
@@ -45,10 +66,12 @@ public class UiLogic : MonoBehaviour
         }
     }
 
-    public void StartGame()
+    public void SwitchScene(int id)
     {
+        string sceneToLoadName = sceneNames[id]; //so i don't need 2 funtion /:
+
         currentUi.SetActive(false); //so u don't see this
-        StartCoroutine(LoadScene(startSceneName)); //loadingScreen need wait
+        StartCoroutine(LoadScene(sceneToLoadName)); //loadingScreen need wait
     }
 
     public void QuitGame()
