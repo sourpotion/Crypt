ee8c17f61232b89cdef5581dc61c3ba220a03688
DDDSchool <97126717@st.deltion.nl>
2026-02-14 21:00:35 +0100

    uiLogic Update
    synax update
    keybinds

--- Assets/Scripts/UiLogic.cs
+++ Assets/Scripts/UiLogic.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections;
+using JetBrains.Annotations;
+using TMPro;
 using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -22,31 +25,54 @@ public class UiLogic : MonoBehaviour
     [Header("Settings")]
     public bool inGame;
 
-    [Serializable]
+    [System.Serializable]
     public class Audios
     {
-        public string soundEffectName;
-        public Slider slider;
-    } 
+        public string audioName; //name of the exposed parameter of the audio 
+        public Slider slider; //slider that say the value of the sound
+    }
+
+    [System.Serializable]
+    public class KeyRebinding
+    {
+        public string actionName; //binding of the keybind like pause or jump
+        public TextMeshProUGUI text; //text to change when using the rebindFunction
+        public Button buttonToActive; //active button
+    }
+
     public Audios[] audios; 
+    public KeyRebinding[] keyRebinding; //so it can be save and loaded
 
     private GameObject currentUi; 
     private String[] sceneNames = {"SampleScene", "SampleScene"}; // id of the scene first is 0 sconf is 1 so on
     private bool gameIsPause = false; //so when it is pause with esc u can go out
     private float maxVolume = 20;
     private float minVolume = -80;
+    private PlayerController plrControl;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        plrControl = new PlayerController();
+
+        OnEnable();
+    }
+
     void Start()
     {
         currentUi = startSchrem;
 
         foreach (Audios audioInfo in audios)
         {
-            audioInfo.slider.onValueChanged.AddListener((value) => {ChangeSound(audioInfo.slider.value, audioInfo.soundEffectName);}); //add the functionEvent to the slider
+            audioInfo.slider.onValueChanged.AddListener((value) => {ChangeSound(audioInfo.slider.value, audioInfo.audioName);}); //add the functionEvent to the slider
         }
 
-        Reset(); //resetTheAudios
+        foreach (KeyRebinding info in keyRebinding)
+        {
+            info.buttonToActive.onClick.AddListener(() => {RebindKey(info);}); //but the action of rebind into the button
+        }
+
+        Reset(); //resetTheAudios and the keybinds
 
         if (currentUi == null) { Debug.LogWarning("u need to do a public startSchrem AUB"); } //{} is more readable* 
     }
@@ -54,11 +80,32 @@ public class UiLogic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+    }
+    
+    //on press
+    
+    void OnEnable()  //little ai but it make it enable
+    {
+        plrControl.Enable();
+        plrControl.Player.Pause.performed += OnPauseButton;
+    }
+
+    void OnDisable() //disable key
+    {
+        plrControl.Player.Pause.performed -= OnPauseButton;
+        plrControl.Disable();
+    }
+
+    private void OnPauseButton(InputAction.CallbackContext context)
+    {
         if (inGame)
         {
-            if (Input.GetKeyDown(KeyCode.Escape)) {TogglePauseGame();}
+            TogglePauseGame();
         }
     }
+
+    //functions
 
     public void ChangeSound(float newValue, string soundName)
     {
@@ -119,17 +166,58 @@ public class UiLogic : MonoBehaviour
         currentUi = newUi; //make the old one the new one
     }
 
-    //saving
+    void RebindKey(KeyRebinding info)
+    {
+        InputAction action = plrControl.asset.FindAction(info.actionName);
+        info.text.text = "...";
+        
+        // Disable action before rebinding
+        action.Disable();
+
+        action.PerformInteractiveRebinding()
+            .OnComplete(operation => //wait for awner
+            {
+                operation.Dispose(); //make the keybind true
+                action.Enable();
+                
+                // put the text to the new key
+                string keybindName = action.bindings[0].ToDisplayString(); //get the string of the new key
+                info.text.text = keybindName;
+
+                //save it
+                string bindingPath = action.bindings[0].effectivePath;
+                PlayerPrefs.SetString(info.actionName + "Keybind", bindingPath);
+                PlayerPrefs.Save();
+            })
+            .Start();
+
+    }
+
+    //saving and loading
 
     void Reset()
     {
         if (!GameMangeren.Instance.audioSaveAlreadyDone) //if not loaded then load 
         {
+            //load everything
             foreach (Audios audiosInfo in audios)
             {
-                float newValue = LoadAudioVolume(audiosInfo.soundEffectName + "Audio"); //load the current value of audio
+                float newValue = LoadAudioVolume(audiosInfo.audioName + "Audio"); //load the current value of audio
                 audiosInfo.slider.value = newValue;
-                ChangeSound(newValue, audiosInfo.soundEffectName);   //changeItInAudio
+                ChangeSound(newValue, audiosInfo.audioName);   //changeItInAudio
+            }
+
+            foreach (KeyRebinding info in keyRebinding)
+            {
+                var action = plrControl.asset.FindAction(info.actionName);
+                string keybindPath = LoadKeyBinds(info.actionName + "Keybind");
+                
+                if (!string.IsNullOrEmpty(keybindPath))
+                {
+                    action.ApplyBindingOverride(0, keybindPath); //pput the newOne in here
+                }
+
+                info.text.text = action.bindings[0].ToDisplayString(); //show the current keybind 
             }
 
             GameMangeren.Instance.audioSaveAlreadyDone = true; //debounce
@@ -139,10 +227,17 @@ public class UiLogic : MonoBehaviour
             foreach (Audios audiosInfo in audios) //but the slider good
             {
                 float currentVolume;
-                if (!audioMixer.GetFloat(audiosInfo.soundEffectName, out currentVolume)) {currentVolume = 0f;} //get the volume of the audio
+                if (!audioMixer.GetFloat(audiosInfo.audioName, out currentVolume)) {currentVolume = 0f;} //get the volume of the audio
                 float newValue = Mathf.InverseLerp(minVolume, maxVolume, currentVolume); //reverse to get value
 
                 audiosInfo.slider.value = newValue;
+            }
+
+            foreach (KeyRebinding info in keyRebinding)
+            {
+                var action = plrControl.asset.FindAction(info.actionName);
+
+                info.text.text = action.bindings[0].ToDisplayString(); //show the current keybind 
             }
         }
     } 
@@ -151,7 +246,7 @@ public class UiLogic : MonoBehaviour
     {
         foreach (Audios audiosInfo in audios)
         {
-            SaveAudioVolume(audiosInfo.soundEffectName + "Audio", audiosInfo.slider.value);
+            SaveAudioVolume(audiosInfo.audioName + "Audio", audiosInfo.slider.value);
         }
     }
 
@@ -162,7 +257,11 @@ public class UiLogic : MonoBehaviour
 
     float LoadAudioVolume(string key) //key = nameOfTheparameter + Audio
     {
-        
         return PlayerPrefs.GetFloat(key, 1f);
     }
+    string LoadKeyBinds(string key) //key == nameOfTheAction + Keybind
+    {
+        return PlayerPrefs.GetString(key, "");
+    }
+
 }
